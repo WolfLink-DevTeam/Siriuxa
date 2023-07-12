aa8e5cb9c866c349f179d41bed666b1fb5393888
MikkoAyaka <3401286177@qq.com>
2023-07-12 22:35:57 +0800

    补充了指令注册

--- src/main/java/priv/mikkoayaka/minecraft/plugin/seriuxajourney/SeriuxaJourney.java
+++ src/main/java/priv/mikkoayaka/minecraft/plugin/seriuxajourney/SeriuxaJourney.java
@@ -3,6 +3,7 @@ package priv.mikkoayaka.minecraft.plugin.seriuxajourney;
 import lombok.Getter;
 import org.wolflink.common.ioc.IOC;
 import org.wolflink.minecraft.wolfird.framework.WolfirdPlugin;
+import org.wolflink.minecraft.wolfird.framework.command.WolfirdCommandAnalyser;
 import priv.mikkoayaka.minecraft.plugin.seriuxajourney.api.VaultAPI;
 import priv.mikkoayaka.minecraft.plugin.seriuxajourney.command.Debug;
 
@@ -15,7 +16,10 @@ public final class SeriuxaJourney extends WolfirdPlugin {
     public void afterEnabled() {
         instance = this;
         IOC.getBean(VaultAPI.class); // 初始化 VaultAPI
-        bindCommand(IOC.getBean(Debug.class)); // 注册指令
+
+        // 注册指令
+        IOC.getBean(WolfirdCommandAnalyser.class).register("sj");
+        bindCommand(IOC.getBean(Debug.class));
     }
 
     @Override
