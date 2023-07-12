7d9f0ac3d2b91ac0dc70dee836e47bff4dd5554d
MikkoAyaka <3401286177@qq.com>
2023-07-12 10:17:22 +0800

    初始化

---
+++ src/main/java/priv/mikkoayaka/minecraft/plugin/seriuxajourney/SeriuxaJourney.java
@@ -0,0 +1,17 @@
+package priv.mikkoayaka.minecraft.plugin.seriuxajourney;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SeriuxaJourney extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
