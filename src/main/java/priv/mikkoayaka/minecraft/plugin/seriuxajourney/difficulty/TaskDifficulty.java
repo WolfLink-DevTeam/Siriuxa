d452a6d7d8cc73c6a3ac50d6f1f4fc2a9b73275f
MikkoAyaka <3401286177@qq.com>
2023-07-12 14:20:04 +0800

    调整难度数据类的部分属性

--- src/main/java/priv/mikkoayaka/minecraft/plugin/seriuxajourney/difficulty/TaskDifficulty.java
+++ src/main/java/priv/mikkoayaka/minecraft/plugin/seriuxajourney/difficulty/TaskDifficulty.java
@@ -1,21 +1,23 @@
 package priv.mikkoayaka.minecraft.plugin.seriuxajourney.difficulty;
 
 /**
- * @param level             难度等级
- * @param name              难度名称
- * @param wheatCost         成本
- * @param wheatLostMultiple 麦穗流失倍率
- * @param damageMultiple    伤害倍率
- * @param bringSlotAmount   带回格数
- * @param wheatGainPercent  麦穗转化率
- * @param expGainPercent    经验转化率
+ * @param level                     难度等级
+ * @param name                      难度名称
+ * @param wheatCost                 成本
+ * @param wheatLostAcceleratedSpeed 麦穗流速加快倍率(每5分钟)
+ * @param hurtWheatCost             受伤麦穗惩罚(每1点伤害)
+ * @param hurtDamageMultiple            伤害倍率
+ * @param bringSlotAmount           带回格数
+ * @param wheatGainPercent          麦穗转化率
+ * @param expGainPercent            经验转化率
  */
 public record TaskDifficulty(
         int level,
         String name,
         int wheatCost,
-        double wheatLostMultiple,
-        double damageMultiple,
+        double wheatLostAcceleratedSpeed,
+        double hurtWheatCost,
+        double hurtDamageMultiple,
         int bringSlotAmount,
         double wheatGainPercent,
         double expGainPercent) {
