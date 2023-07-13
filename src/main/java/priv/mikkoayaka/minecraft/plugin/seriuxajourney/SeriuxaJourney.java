2ca8478ea9f59282926f23494289884027ef028a
MikkoAyaka <3401286177@qq.com>
2023-07-13 17:55:05 +0800

    修复了配置文件未能正确加载的问题

--- src/main/java/priv/mikkoayaka/minecraft/plugin/seriuxajourney/SeriuxaJourney.java
+++ src/main/java/priv/mikkoayaka/minecraft/plugin/seriuxajourney/SeriuxaJourney.java
@@ -9,6 +9,8 @@ import priv.mikkoayaka.minecraft.plugin.seriuxajourney.api.VaultAPI;
 import priv.mikkoayaka.minecraft.plugin.seriuxajourney.api.view.MenuEventListener;
 import priv.mikkoayaka.minecraft.plugin.seriuxajourney.command.Debug;
 import priv.mikkoayaka.minecraft.plugin.seriuxajourney.command.OpenTaskMenu;
+import priv.mikkoayaka.minecraft.plugin.seriuxajourney.file.Config;
+import priv.mikkoayaka.minecraft.plugin.seriuxajourney.file.Lang;
 import priv.mikkoayaka.minecraft.plugin.seriuxajourney.papi.TaskVariables;
 
 public final class SeriuxaJourney extends WolfirdPlugin {
@@ -19,6 +21,10 @@ public final class SeriuxaJourney extends WolfirdPlugin {
     @Override
     public void afterEnabled() {
         instance = this;
+        // 加载配置文件和语言文件
+        IOC.getBean(Config.class).load();
+        IOC.getBean(Lang.class).load();
+
         IOC.getBean(VaultAPI.class); // 初始化 VaultAPI
 
         // 菜单事件处理
