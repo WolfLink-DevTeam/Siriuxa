4d668f6174856908ff0a7df3ef435c4baa597ec6
MikkoAyaka <3401286177@qq.com>
2023-07-19 15:18:49 +0800

    调整任务记录按钮显示样式

--- src/main/java/org/wolflink/minecraft/plugin/siriuxa/menu/task/icon/TaskRecordIcon.java
+++ src/main/java/org/wolflink/minecraft/plugin/siriuxa/menu/task/icon/TaskRecordIcon.java
@@ -8,8 +8,6 @@ import org.wolflink.minecraft.plugin.siriuxa.api.view.Icon;
 import org.wolflink.minecraft.plugin.siriuxa.file.database.PlayerTaskRecord;
 
 import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 
@@ -24,13 +22,16 @@ public class TaskRecordIcon extends Icon {
     protected @NonNull ItemStack createIcon() {
         Date taskFinishedDate = new Date(playerTaskRecord.getFinishedTimeInMills());
         DateFormat dateFormat = DateFormat.getDateInstance(DateFormat.MEDIUM, Locale.CHINA);
+        DateFormat timeFormat = DateFormat.getTimeInstance(DateFormat.MEDIUM, Locale.CHINA);
         String date = dateFormat.format(taskFinishedDate);
-        String iconName = "§8[ §f任务记录 8] §7%s";
+        String iconName = "§8[ §f任务记录 §8] §7%s";
         iconName = String.format(iconName,date);
         String taskResult = playerTaskRecord.isSuccess() ? "§a完成" : "§c失败";
         int minutes = (int) (playerTaskRecord.getUsingTimeInMills() / 60000);
         String claimStatus = playerTaskRecord.isClaimed() ? "§7物资已领取" : "§a可领取物资";
         return fastCreateItemStack(Material.PAPER,1,iconName,
+                " ",
+                "  §7完成时间 §f"+timeFormat.format(taskFinishedDate),
                 " ",
                 "  §7任务类型 §f"+playerTaskRecord.getTaskType(),
                 "  §7任务难度 §f"+playerTaskRecord.getTaskDifficulty(),
