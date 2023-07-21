84c06a4cf6565532c84373a3e145c62d32ba4aaf
MikkoAyaka <3401286177@qq.com>
2023-07-21 12:46:29 +0800

    开局检测海洋，尽量不生成在海洋上

--- src/main/java/org/wolflink/minecraft/plugin/siriuxa/api/world/RegionAPI.java
+++ src/main/java/org/wolflink/minecraft/plugin/siriuxa/api/world/RegionAPI.java
@@ -2,20 +2,28 @@ package org.wolflink.minecraft.plugin.siriuxa.api.world;
 
 import org.bukkit.Location;
 import org.bukkit.World;
+import org.wolflink.common.ioc.IOC;
 import org.wolflink.common.ioc.Inject;
 import org.wolflink.common.ioc.Singleton;
 import org.wolflink.minecraft.plugin.siriuxa.file.Config;
 import org.wolflink.minecraft.plugin.siriuxa.file.ConfigProjection;
+import org.wolflink.minecraft.plugin.siriuxa.utils.Notifier;
 
 @Singleton
 public class RegionAPI {
     @Inject
     Config config;
 
+
+    public Location autoGetRegionCenter(World world) {
+        return autoGetRegionCenter(world,3);
+    }
     /**
      * 根据区块索引指针划分区块区域
+     * @param world 指定世界
+     * @param retry 重试次数
      */
-    public Location autoGetRegionCenter(World world) {
+    private Location autoGetRegionCenter(World world,int retry) {
         int index = config.getNextRegionIndex();
         int spacingRadius = config.get(ConfigProjection.EXPLORATION_REGION_SPACING_RADIUS);
         double radius = config.get(ConfigProjection.EXPLORATION_REGION_RADIUS);
@@ -27,6 +35,11 @@ public class RegionAPI {
         int centerX = -maxLength + x * finalRadius;
         int centerZ = -maxLength + z * finalRadius;
         int y = world.getHighestBlockYAt(centerX, centerZ);
-        return new Location(world, centerX, y, centerZ);
+        Location location = new Location(world, centerX, y, centerZ);
+        if(retry > 0 && IOC.getBean(BlockAPI.class).checkIsOcean(location)) {
+            Notifier.debug("检测到海洋：X "+location.getBlockX()+" Z "+location.getBlockZ());
+            return autoGetRegionCenter(world,retry-1);
+        }
+        return location;
     }
 }
