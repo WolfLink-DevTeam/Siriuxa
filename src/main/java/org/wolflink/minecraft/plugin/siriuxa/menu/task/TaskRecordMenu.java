f6fc72743208784e093120bacaef1c8397adaa2d
MikkoAyaka <3401286177@qq.com>
2023-07-22 10:10:14 +0800

    任务记录菜单支持翻页

--- src/main/java/org/wolflink/minecraft/plugin/siriuxa/menu/task/TaskRecordMenu.java
+++ src/main/java/org/wolflink/minecraft/plugin/siriuxa/menu/task/TaskRecordMenu.java
@@ -1,11 +1,16 @@
 package org.wolflink.minecraft.plugin.siriuxa.menu.task;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.wolflink.common.ioc.IOC;
 import org.wolflink.minecraft.plugin.siriuxa.api.view.Menu;
 import org.wolflink.minecraft.plugin.siriuxa.file.database.PlayerTaskRecord;
 import org.wolflink.minecraft.plugin.siriuxa.file.database.TaskRecordDB;
+import org.wolflink.minecraft.plugin.siriuxa.menu.task.icon.NextPage;
+import org.wolflink.minecraft.plugin.siriuxa.menu.task.icon.PreviousPage;
 import org.wolflink.minecraft.plugin.siriuxa.menu.task.icon.TaskRecordIcon;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 import java.util.UUID;
@@ -22,16 +27,41 @@ public class TaskRecordMenu extends Menu {
         super(ownerUuid, -1, "§0§l任务记录", 54);
     }
 
+    List<PlayerTaskRecord> totalRecordList = new ArrayList<>();
+    @Getter
+    private int page = 1;
+
+    public void setPage(int value) {
+        this.page = value;
+        overrideIcons();
+    }
+
     @Override
     protected void overrideIcons() {
+        setIcon(45,new PreviousPage(this));
+        setIcon(53,new NextPage(this));
         int startIndex = 10;
-        List<PlayerTaskRecord> recordList = IOC.getBean(TaskRecordDB.class).loadRecords(getOfflineOwner().getName());
-        recordList.sort((r1,r2) -> ((int)(r1.getFinishedTimeInMills()-r2.getFinishedTimeInMills())/1000));
-        int maxLen = Math.min(recordList.size(), 28);
-        for (int i = 0; i < maxLen; i++) {
-            PlayerTaskRecord playerTaskRecord = recordList.get(i);
+        totalRecordList = IOC.getBean(TaskRecordDB.class).loadRecords(getOfflineOwner().getName());
+        totalRecordList.sort((r1,r2) -> ((int)(r1.getFinishedTimeInMills()-r2.getFinishedTimeInMills())/1000));
+        int recordSize = totalRecordList.size();
+
+        // 闭区间 [pageFirstRecord,pageLastRecord]
+        int pageFirstRecord = page * 28 - 28;
+        int pageLastRecord = pageFirstRecord + 27;
+
+        if(pageFirstRecord >= recordSize) return;
+
+        List<PlayerTaskRecord> pageRecordList = totalRecordList.subList(pageFirstRecord,Math.min(pageLastRecord,recordSize-1));
+        for (int i = 0; i < pageRecordList.size(); i++) {
+            PlayerTaskRecord playerTaskRecord = pageRecordList.get(i);
             setIcon(startIndex+i,new TaskRecordIcon(playerTaskRecord));
             if((i+1)%7 == 0)startIndex += 2;
         }
+    }
+    public boolean hasPreviousPage() {
+        return page > 1;
+    }
+    public boolean hasNextPage() {
+        return page * 28 < totalRecordList.size();
     }
 }
---
+++ src/main/java/org/wolflink/minecraft/plugin/siriuxa/menu/task/icon/NextPage.java
@@ -0,0 +1,31 @@
+package org.wolflink.minecraft.plugin.siriuxa.menu.task.icon;
+
+import lombok.NonNull;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.wolflink.minecraft.plugin.siriuxa.api.view.Icon;
+import org.wolflink.minecraft.plugin.siriuxa.menu.task.TaskRecordMenu;
+
+public class NextPage extends Icon {
+    private final TaskRecordMenu taskRecordMenu;
+    public NextPage(TaskRecordMenu taskRecordMenu) {
+        super(false);
+        this.taskRecordMenu = taskRecordMenu;
+    }
+
+    @Override
+    protected @NonNull ItemStack createIcon() {
+        return fastCreateItemStack(Material.ENDER_PEARL,1,"下一页");
+    }
+
+    @Override
+    public void leftClick(Player player) {
+        if(taskRecordMenu.hasNextPage()) taskRecordMenu.setPage(taskRecordMenu.getPage()+1);
+    }
+
+    @Override
+    public void rightClick(Player player) {
+
+    }
+}
