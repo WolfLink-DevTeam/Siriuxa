26ab3f8dce8fe46cc60e465d38dce22cc665d711
Amog晴天 <114559286+q1356492075@users.noreply.github.com>
2023-07-23 11:42:09 +0800

    Update ItemAPI.java

--- src/main/java/org/wolflink/minecraft/plugin/siriuxa/api/ItemAPI.java
+++ src/main/java/org/wolflink/minecraft/plugin/siriuxa/api/ItemAPI.java
@@ -11,7 +11,7 @@ import java.util.List;
 public class ItemAPI {
 
     //设置物品的名字并且根据品质物品的颜色（颜色代码通过枚举类来获得）
-    public ItemStack setItemName(ItemStack itemStack,Tier tier, String name) {
+    public ItemStack setItemName(ItemStack itemStack, Tier tier, String name) {
         ItemMeta itemMeta = itemStack.getItemMeta();
         if (itemMeta != null) {
             itemMeta.setDisplayName(tier.getColor() + name);
@@ -20,17 +20,17 @@ public class ItemAPI {
         return itemStack;
     }
 
-    //设置
-    public ItemStack setItemLore(ItemStack itemStack,List<String> lores) {
+    //设置物品描述信息
+    public ItemStack setItemLore(ItemStack itemStack, List<String> lores) {
         ItemMeta itemMeta = itemStack.getItemMeta();
-        if(itemMeta == null) {
-            Notifier.warn("物品 "+itemStack.getType().name()+" 没有 ItemMeta 属性");
+        if (itemMeta == null) {
+            Notifier.warn("物品 " + itemStack.getType().name() + " 没有 ItemMeta 属性");
             return itemStack;
         }
         List<String> finalLores = new ArrayList<>();
-        finalLores.add(" "); // This is blank , not null
-        finalLores.addAll(lores.stream().map(str -> "§7"+str).toList()); // add §7 color to all lines
-        finalLores.add(" ");
+        finalLores.add(" "); //第一行为空白（blank）
+        finalLores.addAll(lores.stream().map(str -> "§7" + str).toList()); //设置物品描述为默认颜色§7
+        finalLores.add(" ");//最后一行为空白（不是null）
         itemMeta.setLore(finalLores);
         itemStack.setItemMeta(itemMeta);
         return itemStack;
