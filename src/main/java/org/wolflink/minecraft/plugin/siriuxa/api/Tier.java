0bfde813df2893ae3b02f5b10702019e845818fa
Amog晴天 <1356492075@qq.com>
2023-07-24 21:29:33 +0800

    完成了 ItemAPI 部分内容的修正

--- src/main/java/org/wolflink/minecraft/plugin/siriuxa/api/Tier.java
+++ src/main/java/org/wolflink/minecraft/plugin/siriuxa/api/Tier.java
@@ -2,10 +2,7 @@ package org.wolflink.minecraft.plugin.siriuxa.api;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-<<<<<<< HEAD
 
-=======
->>>>>>> origin/master
 @Getter
 @AllArgsConstructor
 public enum Tier {
@@ -14,19 +11,11 @@ public enum Tier {
      * 分别对应颜色符号
      * §f §a §b §d §6
      */
-<<<<<<< HEAD
     COMMON("普通", "§f"),
     UNCOMMON("优秀", "§a"),
     RARE("稀有", "§b"),
     EPIC("史诗", "§d"),
     LEGENDARY("传说", "§6");
-=======
-    COMMON("普通","§f"),
-    UNCOMMON("优秀","§a"),
-    RARE("稀有","§b"),
-    EPIC("史诗","§d"),
-    LEGENDARY("传说","§6");
->>>>>>> origin/master
 
     private final String displayName;
     private final String color;
