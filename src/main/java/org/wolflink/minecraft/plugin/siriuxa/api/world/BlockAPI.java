d9097330193fe72c85f1fe4315e69d64592584de
MikkoAyaka <3401286177@qq.com>
2023-07-28 14:51:46 +0800

    调整了海洋检测算法，比之前更加灵敏了

--- src/main/java/org/wolflink/minecraft/plugin/siriuxa/api/world/BlockAPI.java
+++ src/main/java/org/wolflink/minecraft/plugin/siriuxa/api/world/BlockAPI.java
@@ -4,10 +4,14 @@ import lombok.NonNull;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
+import org.bukkit.block.Block;
 import org.wolflink.common.ioc.Singleton;
+import org.wolflink.minecraft.plugin.siriuxa.api.Notifier;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Singleton
 public class BlockAPI {
@@ -34,7 +38,7 @@ public class BlockAPI {
         return result;
     }
 
-    private static final int OCEAN_RADIUS = 8;
+    private static final int OCEAN_RADIUS = 5;
 
     /**
      * 检查给定坐标是否处于海洋中
@@ -43,16 +47,51 @@ public class BlockAPI {
         if (center.getBlock().getType() != Material.WATER) return false;
         World world = center.getWorld();
         int centerX = center.getBlockX();
-        int centerY = center.getBlockY();
+        int centerY = getWaterLevelY(center);
+        Notifier.debug("水平面："+centerY);
+        if(centerY == -1) {
+            Notifier.debug("未找到坐标所处的水平面："+center.getBlockX()+"|"+center.getBlockY()+"|"+center.getBlockZ());
+            return false;
+        }
         int centerZ = center.getBlockZ();
         for (int x = -OCEAN_RADIUS; x <= OCEAN_RADIUS; x++) {
             for (int z = -OCEAN_RADIUS; z <= OCEAN_RADIUS; z++) {
-                Location tempLoc = new Location(world, centerX + (double) x, centerY, centerZ + (double) z);
+                Location tempLoc = new Location(world, centerX + x, centerY, centerZ + z);
                 if (tempLoc.getBlock().getType() != Material.WATER) {
-                    return true;
+                    return false;
                 }
             }
         }
-        return false;
+        return true;
+    }
+
+    private static final int MAX_WATER_LEVEL_Y = 96;
+    /**
+     * 处在水中的方块材质
+     */
+    private static final Set<Material> waterMaterials = new HashSet<>();
+    static {
+        waterMaterials.add(Material.WATER);
+        waterMaterials.add(Material.SEAGRASS);
+        waterMaterials.add(Material.TALL_SEAGRASS);
+        waterMaterials.add(Material.KELP);
+        waterMaterials.add(Material.BRAIN_CORAL_BLOCK);
+        waterMaterials.add(Material.BUBBLE_CORAL_BLOCK);
+        waterMaterials.add(Material.FIRE_CORAL_BLOCK);
+        waterMaterials.add(Material.TUBE_CORAL_BLOCK);
+        waterMaterials.add(Material.HORN_CORAL_BLOCK);
+    }
+    /**
+     * 获取给定坐标当前所处水域的水平面
+     * -1 则为没有找到
+     */
+    public int getWaterLevelY(Location waterLocation) {
+        Block block = waterLocation.getBlock();
+        while (block.getY() < MAX_WATER_LEVEL_Y) {
+            Block upBlock = block.getRelative(0,1,0);
+            if(!waterMaterials.contains(upBlock.getType())) return block.getY();
+            else block = upBlock;
+        }
+        return -1;
     }
 }
--- src/main/java/org/wolflink/minecraft/plugin/siriuxa/monster/strategy/OceanSpawnStrategy.java
+++ src/main/java/org/wolflink/minecraft/plugin/siriuxa/monster/strategy/OceanSpawnStrategy.java
@@ -4,16 +4,15 @@ import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.attribute.Attribute;
-import org.bukkit.block.Biome;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Monster;
 import org.bukkit.entity.Player;
 import org.wolflink.common.ioc.IOC;
 import org.wolflink.minecraft.plugin.siriuxa.Siriuxa;
 import org.wolflink.minecraft.plugin.siriuxa.api.AttributeAPI;
+import org.wolflink.minecraft.plugin.siriuxa.api.world.BlockAPI;
 import org.wolflink.minecraft.plugin.siriuxa.monster.SpawnerAttribute;
 
-import java.util.List;
 import java.util.Objects;
 import java.util.Random;
 
@@ -21,26 +20,6 @@ import java.util.Random;
 public class OceanSpawnStrategy extends SpawnStrategy {
 
     private final Random random = new Random();
-    private static final List<Biome> oceanBiomes;
-
-    static {
-        oceanBiomes = List.of(
-                Biome.LUSH_CAVES,
-                Biome.SWAMP,
-                Biome.MANGROVE_SWAMP,
-                Biome.BEACH,
-                Biome.RIVER,
-                Biome.FROZEN_RIVER,
-                Biome.OCEAN,
-                Biome.COLD_OCEAN,
-                Biome.WARM_OCEAN,
-                Biome.DEEP_OCEAN,
-                Biome.FROZEN_OCEAN,
-                Biome.LUKEWARM_OCEAN,
-                Biome.DEEP_COLD_OCEAN,
-                Biome.DEEP_FROZEN_OCEAN,
-                Biome.DEEP_LUKEWARM_OCEAN);
-    }
 
     private static final int SAFE_RADIUS = 8;
 
@@ -50,9 +29,7 @@ public class OceanSpawnStrategy extends SpawnStrategy {
 
     @Override
     public boolean isApplicable(Player player) {
-        Biome biome = player.getLocation().getBlock().getBiome();
-        return (oceanBiomes.contains(biome) &&
-                player.getLocation().getBlock().isLiquid());
+        return IOC.getBean(BlockAPI.class).checkIsOcean(player.getLocation());
     }
     @Override
     void spawn(Player player, final int triedCount) {
