93568089de8f88182650c1808261c802f5ff0d31
MikkoAyaka <3401286177@qq.com>
2023-07-29 11:47:16 +0800

    测试后修复了菜单部分的一些细节问题

--- src/main/java/org/wolflink/minecraft/plugin/siriuxa/menu/task/icon/ClaimTaskReward.java
+++ src/main/java/org/wolflink/minecraft/plugin/siriuxa/menu/task/icon/ClaimTaskReward.java
@@ -11,7 +11,7 @@ public class ClaimTaskReward extends Icon {
     private final ExplorationBackpackMenu explorationBackpackMenu;
 
     public ClaimTaskReward(ExplorationBackpackMenu explorationBackpackMenu) {
-        super(0);
+        super(10);
         this.explorationBackpackMenu = explorationBackpackMenu;
     }
 
