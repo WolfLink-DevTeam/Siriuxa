a4536cc77dec3bc3a8d3a0b27ef474e28ce7e73f
Amog晴天 <1356492075@qq.com>
2023-08-02 11:38:57 +0800

    更正了与战利品相关的指令的代码

--- src/main/java/org/wolflink/minecraft/plugin/siriuxa/command/ReloadLoot.java
+++ src/main/java/org/wolflink/minecraft/plugin/siriuxa/command/ReloadLoot.java
@@ -1,44 +1,26 @@
 package org.wolflink.minecraft.plugin.siriuxa.command;
 
 import org.bukkit.command.CommandSender;
-import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
-import org.bukkit.plugin.java.JavaPlugin;
+import org.wolflink.common.ioc.Inject;
 import org.wolflink.common.ioc.Singleton;
+import org.wolflink.minecraft.plugin.siriuxa.loot.LootService;
 import org.wolflink.minecraft.wolfird.framework.bukkit.WolfirdCommand;
-
-import java.io.File;
-import java.io.IOException;
 
 /**
  * 重载有关于自定义战利品的配置文件
  */
 @Singleton
 public class ReloadLoot extends WolfirdCommand {
-    private final JavaPlugin plugin;
-    private final File configFile;
-    private FileConfiguration config;
+    @Inject
+    private LootService lootService;
 
-    public ReloadLoot(JavaPlugin plugin) {
+    public ReloadLoot() {
         super(true, false, false, "sx reloadloot", "重载有关于自定义战利品的配置文件");
-        this.plugin = plugin;
-        configFile = new File(plugin.getDataFolder(), "loot.yml");
-        config = YamlConfiguration.loadConfiguration(configFile);
     }
 
     @Override
     protected void execute(CommandSender commandSender, String[] strings) {
-        if (!(commandSender instanceof Player)) return; // 只有Player才可以运行这个命令
-        if (!configFile.exists()) {
-            plugin.saveResource("loot.yml", false);
-        }
-        config = YamlConfiguration.loadConfiguration(configFile);
-        try {
-            config.save(configFile); // 保存配置文件
-        } catch (IOException e) {
-            return;
-        }
-        commandSender.sendMessage("配置文件重载成功");
+        lootService.reloadLoot();
     }
 }
---
+++ src/main/java/org/wolflink/minecraft/plugin/siriuxa/file/database/LootDB.java
@@ -0,0 +1,58 @@
+package org.wolflink.minecraft.plugin.siriuxa.file.database;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.EntityType;
+import org.bukkit.inventory.ItemStack;
+import org.wolflink.common.ioc.Singleton;
+import org.wolflink.minecraft.plugin.siriuxa.api.Notifier;
+
+import java.io.File;
+import java.util.Map;
+
+@Singleton
+public class LootDB extends FileDB {
+    public LootDB() {
+        super("loot");
+    }
+
+    public void reload() {
+        File lootFile = new File(folder, "loot.yml");
+        FileConfiguration fileConfiguration = getFileConfiguration(lootFile);
+        if (fileConfiguration == null) fileConfiguration = createAndLoad(lootFile);
+        try {
+            fileConfiguration.save(lootFile);
+            Notifier.debug("配置文件加载成功。");
+        } catch (Exception e) {
+            e.printStackTrace();
+            Notifier.debug("配置文件加载失败。");
+        }
+    }
+
+    public void setLoot(EntityType entityType, String loot) {
+        File lootFile = new File(folder, entityType.name() + ".yml");
+        FileConfiguration fileConfiguration = getFileConfiguration(lootFile);
+        if (fileConfiguration == null) fileConfiguration = createAndLoad(lootFile);
+        fileConfiguration.set(entityType.name().toLowerCase(), loot);
+        try {
+            fileConfiguration.save(lootFile);
+            Notifier.debug("实体类型为" + entityType.name() + "的战利品为" + loot + " 已保存。");
+        } catch (Exception e) {
+            e.printStackTrace();
+            Notifier.debug("尝试保存 实体类型为" + entityType.name() + "的战利品为" + loot + " 失败。");
+        }
+    }
+
+    public void setDropChance(Map<String, ItemStack> loot, double dropChance) {
+        File lootFile = new File(folder, loot + ".yml");
+        FileConfiguration fileConfiguration = getFileConfiguration(lootFile);
+        if (fileConfiguration == null) fileConfiguration = createAndLoad(lootFile);
+        fileConfiguration.set(loot.toString(), dropChance);
+        try {
+            fileConfiguration.save(lootFile);
+            Notifier.debug("战利品为" + loot + "的掉落概率为" + dropChance + " 已保存。");
+        } catch (Exception e) {
+            e.printStackTrace();
+            Notifier.debug("尝试保存 战利品为" + loot + "的掉落概率为" + dropChance + " 失败。");
+        }
+    }
+}
