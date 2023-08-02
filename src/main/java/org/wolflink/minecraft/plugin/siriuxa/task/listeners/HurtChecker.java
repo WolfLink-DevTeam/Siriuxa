24ee6c57782a841d1949faf8c803c9b0ec9aba83
MikkoAyaka <3401286177@qq.com>
2023-08-02 21:29:34 +0800

    增加了1秒的受伤无敌时间

--- src/main/java/org/wolflink/minecraft/plugin/siriuxa/task/listeners/HurtChecker.java
+++ src/main/java/org/wolflink/minecraft/plugin/siriuxa/task/listeners/HurtChecker.java
@@ -1,12 +1,16 @@
 package org.wolflink.minecraft.plugin.siriuxa.task.listeners;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 import org.wolflink.common.ioc.Inject;
 import org.wolflink.common.ioc.Singleton;
+import org.wolflink.minecraft.plugin.siriuxa.Siriuxa;
 import org.wolflink.minecraft.plugin.siriuxa.task.tasks.common.Task;
 import org.wolflink.minecraft.plugin.siriuxa.task.tasks.common.TaskRepository;
 import org.wolflink.minecraft.plugin.siriuxa.task.tasks.wheat.WheatTask;
@@ -30,6 +34,11 @@ public class HurtChecker extends WolfirdListener {
         if (player.getWorld() != task.getTaskArea().getCenter().getWorld()) return; // 不在任务世界
         // 下调大额伤害
         if (event.getDamage() > 12) event.setDamage(12);
+        // 受伤无敌时间1秒
+        Bukkit.getScheduler().runTaskLater(Siriuxa.getInstance(),
+                ()-> player.addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE,
+                        20,4,false,false,false)),1);
+
         // 扣除麦穗
         double cost = wheatTask.getHurtWheatCost() * event.getFinalDamage();
         wheatTask.takeWheat(cost);
