5c6a902c51367903d40b8c38b51b4676ef6b4b70
Eight_Eggs <asaka8@proton.me>
2023-08-04 16:34:04 +0800

    修复了弹射物不触发伤害保护的bug

--- src/main/java/org/wolflink/minecraft/plugin/siriuxa/task/listeners/hurtcheck/HurtCheckerConfig.java
+++ src/main/java/org/wolflink/minecraft/plugin/siriuxa/task/listeners/hurtcheck/HurtCheckerConfig.java
@@ -11,12 +11,11 @@ public class HurtCheckerConfig {
     /**
      * 无敌时间(单位：tick)
      */
-    static final long INVULNERABLE_TICKS = 21;
+    static final long INVULNERABLE_TICKS = 35;
     /**
      * 排除的伤害类型(不会触发无敌)
      */
     static final Set<EntityDamageEvent.DamageCause> excludeDamageCause = EnumSet.of(
-            EntityDamageEvent.DamageCause.KILL,
             EntityDamageEvent.DamageCause.VOID,
             EntityDamageEvent.DamageCause.LAVA,
             EntityDamageEvent.DamageCause.FIRE,
@@ -28,7 +27,6 @@ public class HurtCheckerConfig {
             EntityDamageEvent.DamageCause.HOT_FLOOR,
             EntityDamageEvent.DamageCause.FIRE_TICK,
             EntityDamageEvent.DamageCause.STARVATION,
-            EntityDamageEvent.DamageCause.PROJECTILE,
             EntityDamageEvent.DamageCause.FALLING_BLOCK
     );
 
