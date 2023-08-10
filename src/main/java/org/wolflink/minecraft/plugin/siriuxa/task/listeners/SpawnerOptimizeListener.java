6a562fac1183e4b2084fce2cabe59ac0666662a3
Eight_Eggs <asaka8@proton.me>
2023-08-10 10:09:09 +0800

    尝试优化刷怪笼性能，使其10分钟后消失

---
+++ src/main/java/org/wolflink/minecraft/plugin/siriuxa/task/listeners/SpawnerOptimizeListener.java
@@ -0,0 +1,30 @@
+package org.wolflink.minecraft.plugin.siriuxa.task.listeners;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.block.CreatureSpawner;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.entity.SpawnerSpawnEvent;
+import org.wolflink.common.ioc.Singleton;
+import org.wolflink.minecraft.plugin.siriuxa.Siriuxa;
+import org.wolflink.minecraft.wolfird.framework.bukkit.WolfirdListener;
+
+@Singleton
+public class SpawnerOptimizeListener extends WolfirdListener {
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onSpawnerSpawn(SpawnerSpawnEvent event) {
+        CreatureSpawner spawner = event.getSpawner();
+        if (spawner.getRequiredPlayerRange() <= 4) return;
+        spawner.setSpawnCount(1);
+        spawner.setSpawnRange(2);
+        spawner.setMinSpawnDelay(200);
+        spawner.setMaxSpawnDelay(200);
+        spawner.setRequiredPlayerRange(4);
+        spawner.update();
+        Bukkit.getScheduler().runTaskLater(Siriuxa.getInstance(), () -> {
+            spawner.setType(Material.AIR);
+            spawner.update();
+        }, 20L * 60 * 10);
+    }
+}
