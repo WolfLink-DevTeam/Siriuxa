ea260cf11c31438a1186a29f191a57b25d8b0990
MikkoAyaka <3401286177@qq.com>
2023-08-10 10:38:50 +0800

    修复了getTaskTeam空指针问题

--- src/main/java/org/wolflink/minecraft/plugin/siriuxa/task/interfaces/ITaskTeam.java
+++ src/main/java/org/wolflink/minecraft/plugin/siriuxa/task/interfaces/ITaskTeam.java
@@ -2,8 +2,10 @@ package org.wolflink.minecraft.plugin.siriuxa.task.interfaces;
 
 import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
+import org.jetbrains.annotations.Nullable;
 import org.wolflink.minecraft.plugin.siriuxa.team.TaskTeam;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -11,17 +13,24 @@ public interface ITaskTeam {
     /**
      * 任务队伍(不可加入) 在任务预加载阶段初始化
      */
+    @Nullable
     TaskTeam getTaskTeam();
     default List<OfflinePlayer> getOfflinePlayers() {
-        return getTaskTeam().getOfflinePlayers();
+        TaskTeam taskTeam = getTaskTeam();
+        if(taskTeam == null) return new ArrayList<>();
+        return taskTeam.getOfflinePlayers();
     }
 
     default int getTaskTeamSize() {
-        return getTaskTeam().size();
+        TaskTeam taskTeam = getTaskTeam();
+        if(taskTeam == null) return 0;
+        return taskTeam.size();
     }
 
     default boolean taskTeamContains(UUID uuid) {
-        return getTaskTeam().contains(uuid);
+        TaskTeam taskTeam = getTaskTeam();
+        if(taskTeam == null) return false;
+        return taskTeam.contains(uuid);
     }
 
     default boolean taskTeamContains(Player player) {
@@ -31,6 +40,8 @@ public interface ITaskTeam {
      * 获取执行任务过程中的所有在线玩家
      */
     default List<Player> getTaskPlayers() {
-        return getTaskTeam().getPlayers();
+        TaskTeam taskTeam = getTaskTeam();
+        if(taskTeam == null) return new ArrayList<>();
+        return taskTeam.getPlayers();
     }
 }
