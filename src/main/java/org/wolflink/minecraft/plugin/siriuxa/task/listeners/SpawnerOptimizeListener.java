d095a9197f11b3cc85c5839b0aa4f4ecd2bb5ea0
Eight_Eggs <asaka8@proton.me>
2023-08-11 16:04:58 +0800

    增加可撤离时在计分板显示坐标的功能

--- src/main/java/org/wolflink/minecraft/plugin/siriuxa/task/listeners/SpawnerOptimizeListener.java
+++ src/main/java/org/wolflink/minecraft/plugin/siriuxa/task/listeners/SpawnerOptimizeListener.java
@@ -17,9 +17,7 @@ public class SpawnerOptimizeListener extends WolfirdListener {
     public void onSpawnerSpawn(SpawnerSpawnEvent event) {
         CreatureSpawner spawner = event.getSpawner();
         Block spawnerBlock = spawner.getBlock();
-        Bukkit.getScheduler().runTaskLater(Siriuxa.getInstance(), () -> {
-            spawnerBlock.setType(Material.AIR);
-        }, 20L * 60 * 10);
+        Bukkit.getScheduler().runTaskLater(Siriuxa.getInstance(), () -> spawnerBlock.setType(Material.AIR), 20L * 60 * 10);
         if (spawner.getRequiredPlayerRange() <= 4) return;
         spawner.setSpawnCount(1);
         spawner.setSpawnRange(2);
