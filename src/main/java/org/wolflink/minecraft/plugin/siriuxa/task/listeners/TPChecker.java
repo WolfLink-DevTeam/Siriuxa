ea10f53f1e70d430b88944f37cc6523eccd759cf
MikkoAyaka <3401286177@qq.com>
2023-08-13 16:43:06 +0800

    新增观察者到达任务世界Y300传送回城的功能

--- src/main/java/org/wolflink/minecraft/plugin/siriuxa/task/listeners/TPChecker.java
+++ src/main/java/org/wolflink/minecraft/plugin/siriuxa/task/listeners/TPChecker.java
@@ -1,13 +1,23 @@
 package org.wolflink.minecraft.plugin.siriuxa.task.listeners;
 
+import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
 import org.bukkit.World;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.player.PlayerTeleportEvent;
+import org.wolflink.common.ioc.IOC;
 import org.wolflink.common.ioc.Singleton;
 import org.wolflink.minecraft.plugin.siriuxa.api.Notifier;
+import org.wolflink.minecraft.plugin.siriuxa.command.GoLobby;
+import org.wolflink.minecraft.plugin.siriuxa.file.Config;
+import org.wolflink.minecraft.plugin.siriuxa.file.ConfigProjection;
 import org.wolflink.minecraft.wolfird.framework.bukkit.WolfirdListener;
 
+/**
+ * 传送相关的检查
+ */
 @Singleton
 public class TPChecker extends WolfirdListener {
     @EventHandler(priority = EventPriority.LOWEST)
@@ -22,5 +32,21 @@ public class TPChecker extends WolfirdListener {
             Notifier.chat("你不能在观察者模式下跨世界传送！", event.getPlayer());
             event.setCancelled(true);
         }
+    }
+
+    @Override
+    public void onEnable() {
+        String taskWorldName = IOC.getBean(Config.class).get(ConfigProjection.EXPLORATION_TASK_WORLD_NAME);
+        getSubScheduler().runTaskTimerAsync(()->{
+            for (Player player : Bukkit.getOnlinePlayers()) {
+                // 玩家在任务世界
+                if(player.getWorld().getName().equals(taskWorldName)) {
+                    // Y >= 300 而且是观察者
+                    if(player.getLocation().getBlockY() >= 300 && player.getGameMode() == GameMode.SPECTATOR) {
+                        IOC.getBean(GoLobby.class).tryExecute(player,null);
+                    }
+                }
+            }
+        },20,20);
     }
 }
