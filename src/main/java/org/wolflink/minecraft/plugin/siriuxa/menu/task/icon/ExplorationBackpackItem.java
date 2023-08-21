5e027ab931ae233814025197ddc4ada32858a98b
MikkoAyaka <3401286177@qq.com>
2023-08-21 09:29:35 +0800

    无法从任务中带回容器

--- src/main/java/org/wolflink/minecraft/plugin/siriuxa/menu/task/icon/ExplorationBackpackItem.java
+++ src/main/java/org/wolflink/minecraft/plugin/siriuxa/menu/task/icon/ExplorationBackpackItem.java
@@ -10,7 +10,9 @@ import org.wolflink.minecraft.plugin.siriuxa.api.view.Icon;
 import org.wolflink.minecraft.plugin.siriuxa.menu.task.ExplorationBackpackMenu;
 
 import javax.annotation.Nullable;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class ExplorationBackpackItem extends Icon {
 
@@ -43,10 +45,25 @@ public class ExplorationBackpackItem extends Icon {
         } else return itemStack;
     }
 
+    private static final Set<String> containerSuffix = new HashSet<>();
+    static {
+        containerSuffix.add("SHULKER_BOX");
+        containerSuffix.add("SHULKER_SHELL");
+        containerSuffix.add("BUNDLE");
+    }
+
     @Override
     public void leftClick(Player player) {
         if (itemStack == null) return;
         if (itemStack.getType() == Material.AIR) return;
+        boolean isContainer = false;
+        for (String containerName : containerSuffix) {
+            if(itemStack.getType().name().endsWith(containerName)){
+                isContainer = true;
+                break;
+            }
+        }
+        if(isContainer) return;
         player.playSound(player.getLocation(), Sound.ENTITY_VILLAGER_YES, 1f, 1f);
         explorationBackpackMenu.selectSlot(index);
     }
