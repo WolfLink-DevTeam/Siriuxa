f07591c166588b98b4094d92a9d2d56e1ebeeef7
MikkoAyaka <3401286177@qq.com>
2023-10-06 10:12:05 +0800

    补充 OrnamentType 注释

--- src/main/java/org/wolflink/minecraft/plugin/siriuxa/task/ornaments/OrnamentType.java
+++ src/main/java/org/wolflink/minecraft/plugin/siriuxa/task/ornaments/OrnamentType.java
@@ -1,5 +1,31 @@
 package org.wolflink.minecraft.plugin.siriuxa.task.ornaments;
 
 public enum OrnamentType {
-    SCULK_INFECTION, SAFE_WORKING, SUPPLIES_COLLECTION, PANDORA_TRIAL, SMART_AI,
+    /**
+     * 幽匿感染
+     * 怪物死亡有概率爆发幽匿感染，世界中也会自然爆发幽匿感染
+     * 玩家在幽匿方块附近会获得感染值，感染值累计到一定程度玩家会获得负面BUFF
+      */
+    SCULK_INFECTION,
+    /**
+     * 安全作业
+     * 玩家可以托运物资进入任务
+     */
+    SAFE_WORKING,
+    /**
+     * 物资收集
+     * 任务中的物资在任务结束后可以带回一部分
+     */
+    SUPPLIES_COLLECTION,
+    /**
+     * 潘多拉试炼
+     * 任务开始后将由房主选择任务tag，增加任务难度并提高任务报酬，
+     * 或者降低任务难度并减少任务报酬。
+     */
+    PANDORA_TRIAL,
+    /**
+     * AI强化
+     * 类似史诗攻城模组
+     */
+    SMART_AI,
 }
